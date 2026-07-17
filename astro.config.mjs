068fb2cbcf2c962e83964a9fb1fb2d000872d1ae
fromanmx-bit <fromanmx@gmail.com>
2026-07-16 19:47:55 -0600

    ci: activar despliegue en GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,8 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  site: 'https://drfelipehs.com',
+  site: 'https://fromanmx-bit.github.io',
+  base: '/dr-felipe-web',
   output: 'static',
   outDir: './dist',
 });
